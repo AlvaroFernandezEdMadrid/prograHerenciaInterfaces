6626840ce855518f331aba5d11c88f04dbef5bff
olivia <olivia.postigo@educa.madrid.org>
2024-03-18 19:24:35 +0100

    Lunes1

--- electrodomestico/Electrodomestico.java
+++ electrodomestico/Electrodomestico.java
@@ -7,6 +7,42 @@ public class Electrodomestico {
 	private Color color;
 	private Consumo consumo;
 	private float peso;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public Consumo getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(Consumo consumo) {
+		this.consumo = consumo;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
 	
 	
 }
