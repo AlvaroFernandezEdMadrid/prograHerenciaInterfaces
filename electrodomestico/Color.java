ba372467c5a32f916e5051195f8050fc1607f092
olivia <olivia.postigo@educa.madrid.org>
2024-03-18 19:23:35 +0100

    Lunes

---
+++ electrodomestico/Color.java
@@ -0,0 +1,5 @@
+package electrodomestico;
+
+public enum Color {
+	BLANCO, NEGRO, ROJO, AZUL, GRIS;
+}
---
+++ electrodomestico/Electrodomestico.java
@@ -0,0 +1,12 @@
+package electrodomestico;
+
+public class Electrodomestico {
+	private String id;
+	private int stock;
+	private float precio;
+	private Color color;
+	private Consumo consumo;
+	private float peso;
+	
+	
+}
